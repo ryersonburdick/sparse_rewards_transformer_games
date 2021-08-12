f20774388d6f93011b593bfe041ceee02dbd4c7f
ryersonburdick <47985084+ryersonburdick@users.noreply.github.com>
2021-08-12 12:49:28 -0500

    Add files via upload

---
+++ src/model/generate.py
@@ -0,0 +1,91 @@
+"""Given a run name and a prefix or a text file of prefix-response pairs, use the prefix to generate responses."""
+
+import gpt_2_simple as gpt2
+import tensorflow as tf
+import os
+import argparse
+
+# Paths to model and checkpoint locations
+MODEL_DIR = "models"
+CHECKPOINT_DIR = "checkpoint"
+# Special tokens indicating start of prompt, start of response, and end of response
+PROMPT_START_TOKEN = "<|startoftext|>[WP]"
+RESPONSE_START_TOKEN = "[RESPONSE]"
+END_TOKEN = "<|endoftext|>"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--run_name", help="Name of existing model run to load and use for generation.", required=True)
+parser.add_argument("--prefix", default=None, 
+    help="Text prefix to use for generation. If specified, ignores --data argument and prints the generated response to this prefix before quitting.", required=False)
+parser.add_argument("--data", default=None, 
+    help="File containing lines in the format <|startoftext|>[WP] (prefix)[RESPONSE](response)<|endoftext|> whose prefixes will be used to generate responses.", required=False)
+parser.add_argument("--output", default=None, help="Name of file to write generated response(s) to. Default is {run_name}_responses.txt", required=False)
+parser.add_argument("--temperature", type=float, default=0.7)
+parser.add_argument("--n_samples", type=int, default=1)
+args = parser.parse_args()
+
+
+def main():
+    # Start session
+    sess = gpt2.start_tf_sess()
+
+    # Load pre-trained/tuned model
+    gpt2.load_gpt2(sess, run_name=args.run_name, checkpoint_dir=CHECKPOINT_DIR)
+
+    # If prefix is specified, print generated response
+    if args.prefix is not None:
+        output = gpt2.generate(
+            sess, 
+            run_name=args.run_name, 
+            checkpoint_dir=CHECKPOINT_DIR, 
+            prefix=args.prefix, 
+            nsamples=args.n_samples, 
+            temperature=args.temperature, 
+            return_as_list=True)[0]
+        print(output)
+
+        # Write output to file, if specified
+        if args.output is not None:
+            with open(args.output, 'w') as file:
+                file.write(args.prefix + output)
+
+    # Otherwise, load data file, separate prefixes from responses, generate output for each prefix, and record
+    else:
+        # Open data file
+        with open(args.data, 'r') as file:
+            samples = [line for line in file.readlines()]
+        
+        # Split each sample into prompt and existing (correct) response
+        split_samples = [sample.split(RESPONSE_START_TOKEN) for sample in samples]
+        prompts = [sample[0] + RESPONSE_START_TOKEN for sample in split_samples]
+        print(prompts)
+        # Generate a new response for each prompt, truncate at the end-of-line token
+        def truncate_response(response):
+            return response.split(END_TOKEN)[0] + END_TOKEN
+        new_pairs = [
+            truncate_response(
+                gpt2.generate(
+                    sess, 
+                    run_name=args.run_name, 
+                    checkpoint_dir=CHECKPOINT_DIR, 
+                    prefix=prompt, 
+                    nsamples=args.n_samples,
+                    temperature=args.temperature,
+                    return_as_list=True
+                )[0]
+            ) for prompt in prompts
+        ]
+        output = "\n".join(new_pairs)
+
+        # If output file not specified, default to {run_name}_responses.txt
+        if args.output is None:
+            output_file = args.run_name + "_responses.txt"
+        else:
+            output_file = args.output
+        
+        with open(output_file, 'w') as file:
+            file.write(output)
+
+
+if __name__ == "__main__":
+    main()
